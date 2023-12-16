1e3ccec7c844c8ef7d3f59384d44feb8799405cb
MerenkovV <merenkovvladislav@mail.ru>
2023-12-16 18:29:00 +0700

    Added "ADMIN" role

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,7 +4,7 @@ const UserController = require('../controllers/UserController')
 const authMiddleware = require('../middleware/authMiddleware');
 const checkRoleMiddleware = require('../middleware/checkRoleMiddleware');
 
-router.post('/registration',authMiddleware, checkRoleMiddleware(['MANAGER']), UserController.registration)
+router.post('/registration',authMiddleware, checkRoleMiddleware(['ADMIN']), UserController.registration)
 router.post('/login', UserController.login)
 router.get('/auth', authMiddleware, UserController.auth)
 
