224b9444849aa1dd26de64260e82cc21cd74ffab
Michael Lyagin <lyaginmikhail@mail.ru>
2023-12-23 03:47:14 +0700

    feat: добавлена таблица на главную страницу

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,40 +1,104 @@
-import React from 'react'
+import { intercept } from 'mobx';
+import React from 'react';
+import './style.css';
 
-export default function Table(type, data) {
-    const headerArray = []
-    switch (type) {
-        case 'unAuth':
+
+const Table = ({type, isAuth, userRole, data}) => {
+    let headerArray = [];
+    let machine = [];
+    machine = data;
+
+    switch (userRole) {
+        case undefined:
+            type === 'info' ? 
             headerArray = [
-                'Зав. № машины', 'Дата отгрузки с завода', 'Модель техники',
-                'Модель двигателя', 'Модель трансмиссии', 'Модель управляемого моста',
-                'Модель ведущего моста'
+                'Зав. № машины', 'Модель техники', 'Модель двигателя', 'Зав. № двигателя',
+                'Модель трансмиссии', 'Зав. № трансмиссии', 'Модель ведущего моста', 'Зав. № ведущего моста',
+                'Модель управляемого моста', 'Зав. № управляемого моста'
+            ] : 
+            type === 'to' ? 
+            headerArray = [
+                'Виды ТО', 'Дата проведения ТО', 'Наработка, м/час', '№ заказ-наряда',
+                'Дата заказ-наряда', 'Организация, проводившая ТО ', 'Машина', 'Сервисная компания'
+            ] :
+            headerArray = [
+                'Дата отказа', 'Наработка, м/час', 'Узел отказа', 'Описание отказа',
+                'Способ восстановления', 'Используемые запасные части', 'Дата восстановления', 'Время простоя техники',
+                'Машина', 'Сервисная компания'
             ]
             break;
-    
+        case 'MANAGER':
+        case 'CLIENT':
+        case 'SERVICE':
+        case 'ADMIN':
+            type === 'info' ? 
+            headerArray = [
+                'Зав. № машины', 'Модель техники', 'Модель двигателя', 'Зав. № двигателя',
+                'Модель трансмиссии', 'Зав. № трансмиссии', 'Модель ведущего моста', 'Зав. № ведущего моста',
+                'Модель управляемого моста', 'Зав. № управляемого моста', 'Договор поставки №, дата',
+                'Дата отгрузки с завода', 'Грузополучатель', 'Адрес поставки', 'Комплектация',
+                'Клиент', 'Сервисная компания'
+            ] : 
+            type === 'to' ? 
+            headerArray = [
+                'Виды ТО', 'Дата проведения ТО', 'Наработка, м/час', '№ заказ-наряда',
+                'Дата заказ-наряда', 'Организация, проводившая ТО ', 'Машина', 'Сервисная компания'
+            ] :
+            headerArray = [
+                'Дата отказа', 'Наработка, м/час', 'Узел отказа', 'Описание отказа',
+                'Способ восстановления', 'Используемые запасные части', 'Дата восстановления', 'Время простоя техники',
+                'Машина', 'Сервисная компания'
+            ]
+            break;
         default:
             break;
     }
+
   return (
-    <table>
-        <thead>
-            <tr className='main-page-info-head-tr'>
-                {headerArray.map(item=><td>{item}</td>)}
-            </tr>
-        </thead>
-        <tbody>
-            {
-                data.map(object=><tr>{}</tr>)
-            }
-            <tr className='main-page-info-tr'>
-                {/* <td>{machine.machine.factoryNumber}</td>
-                <td>{machine.machine.shippingDate}</td>
-                <td>{machine.machine.equipmentModel}</td>
-                <td>{machine.machine.engineModel}</td>
-                <td>{machine.machine.transmissionModel}</td>
-                <td>{machine.machine.driveAxleModel}</td>
-                <td>{machine.machine.steeringAxleModel}</td> */}
-            </tr> 
-        </tbody>
-    </table>
+    <div className='table-block'>
+        <table className='main-table' style={{width: userRole !== undefined && type === 'info' ? '2300px' : '1200px'}}>
+            <thead>
+                <tr className='main-page-info-head-tr'>
+                    {headerArray.map(item=><th>{item}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    {
+                    Array.isArray(machine) ? machine.map( object => <tr>{object.value}</tr>) : null
+                    }
+                </tr>
+                {userRole === undefined && type === 'info' ? 
+                    <tr className='main-page-info-tr'>
+                        <td>{machine.id}</td>
+                        <td>{machine.dictionary.technique_model.name}</td>
+                        <td>{machine.dictionary.engine_model.name}</td>
+                        <td>{machine.engine_number}</td>
+                        <td>{machine.dictionary.transmission_model.name}</td>
+                        <td>{machine.transmission_number}</td>
+                        <td>{machine.dictionary.driving_bridge_model.name}</td>
+                        <td>{machine.driving_bridge_number}</td>
+                        <td>{machine.dictionary.controlled_bridge_model.name}</td>
+                        <td>{machine.controlled_bridge_number}</td>
+                    </tr> :
+                    type === 'info' ? 
+                    <tr className='main-page-info-tr'> 
+                        <td>{machine.id}</td>
+                        <td>{machine.dictionary.technique_model.name}</td>
+                        <td>{machine.dictionary.engine_model.name}</td>
+                        <td>{machine.engine_number}</td>
+                        <td>{machine.dictionary.transmission_model.name}</td>
+                        <td>{machine.transmission_number}</td>
+                        <td>{machine.dictionary.driving_bridge_model.name}</td>
+                        <td>{machine.driving_bridge_number}</td>
+                        <td>{machine.dictionary.controlled_bridge_model.name}</td>
+                        <td>{machine.controlled_bridge_number}</td>
+                    </tr> : null
+                }
+            </tbody>
+        </table>
+    </div>
   )
 }
+
+export default Table
